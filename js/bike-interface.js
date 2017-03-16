a9c754165d8b9d00a9f49c4629c94724407a7ace
Brynna McCarter and Felix Oporto Lopez <felix@>
2017-03-16 16:49:26 -0700

    add almost working maps api

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,5 +1,23 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
+var callback = function(mapData) {
+  console.log(mapData);
+  var uluru = {lat: -25.363, lng: 131.044};
+  var portland = {lat: 45.5230622, lng: -122.6764816};
+  var arrayMaps = [uluru, portland];
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: uluru
+  });
+  for (var i = 0; i < arrayMaps.length; i++) {
+    var marker = new google.maps.Marker({
+
+      position: arrayMaps[i],
+      map: map
+    });
+  }
+}
 $(document).ready(function() {
   $('#search').click(function() {
     var manufacturer = $('#manufacturer').val();
@@ -7,9 +25,8 @@ $(document).ready(function() {
     var location = $('#location').val();
     var distance = $('#distance').val();
     var newBike = new Bike();
-    var result = newBike.search(manufacturer, location, distance);
-    initMap();
-    console.log(result);
+    var locationArray = newBike.search(manufacturer, location, distance, callback);
+
 
   });
 });
--- js/bike.js
+++ js/bike.js
@@ -4,25 +4,35 @@ var apiKey = require('./../.env').apiKey;
 function Bike(){
 }
 
-Bike.prototype.search = function(manufacturer, location, distance) {
+Bike.prototype.search = function(manufacturer, location, distance, callback) {
   var locationArray = [];
+
   $.get('https://bikeindex.org/api/v3/search?page=1&per_page=20&manufacturer=' + manufacturer + '&location=' + location + '%20&distance=' + distance + '&stolenness=proximity').then(function(response) {
 
 
     for (var i = 0; i < response.bikes.length; i++) {
+      var image = null;
+      if (response.bikes[i].thumb) {
+        image = '<img src="' + response.bikes[i].thumb + '">'
+      } else {
+        image = ":  no image found";
+      }
       $('#manufacturer-results').append('<li>' + response.bikes[i].title +
-       response.bikes[i].stolen_location +  '<img src="' + response.bikes[i].large_img + '"></li>');
+       response.bikes[i].stolen_location + image + '</li>');
       var city = response.bikes[i].stolen_location;
+       $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + city +"&key=" + apiKey).then(function(response2) {
+         coordinates = [response2.results[0].geometry.location.lat, response2.results[0].geometry.location.lng];
+         locationArray.push(coordinates);
 
-      var encoded2 =  city.replace(/,/g, "");
-      var encoded =  encoded2.replace(/ /g, "+");
-      // console.log(city);
-      // console.log(encoded);
-       $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + encoded +"&key=" + apiKey).then(function(response) {
-         locationArray.push([response.results[0].geometry.location.lat, response.results[0].geometry.location.lng]);
+
+
        });
-     }
+
+     }.then(function(){
+          callback(locationArray);
+     });
    });
-   return locationArray;
+
+
 };
   exports.bikeModule = Bike;
